75cb6929d3a5e9c14ba026ccc95d3398f7f57e6c
Fernando Felix <fernandojr.ifcg@live.com>
2016-02-10 21:56:08 -0300

    Python 3 supoort.

--- src/lolapi/python/raw.py
+++ src/lolapi/python/raw.py
@@ -2,10 +2,9 @@
 Raw  matches from LOL API using RiotWatcher.
 
 Match filter:
-    matchMode: CLASSIC
     season: SEASON2015
     region: BR
-    queueType: RANKED_SOLO_5x5
+    queueType: RANKED_SOLO_5x5 (matchMode: CLASSIC, participants: 10)
 '''
 
 import json
@@ -17,10 +16,6 @@ api = RiotWatcher(key=config.API_KEY, default_region=BRAZIL)
 starting_match_id = config.STARTING_MATCH_ID  # first match to dump
 
 n_matches = 10000  # total matches to dump
-
-
-def is_classic(match):
-    return match['matchMode'] == 'CLASSIC'
 
 
 def is_season_2015(match):
@@ -36,24 +31,20 @@ def is_ranked_solo_5x5(match):
 
 
 def is_valid(match):
-    return (
-        is_classic(match) and
-        is_season_2015(match) and
-        is_br(match) and
-        is_ranked_solo_5x5(match))
+    return is_season_2015(match) and is_br(match) and is_ranked_solo_5x5(match)
 
 
 counter = 0  # total matches dumped
 i = 0  # matches iterator
 while counter < n_matches:
-    try:  # get_match might have a connection time-out
+    try:  # TODO get_match might have a connection time-out
 
         match_id = i + starting_match_id
-        print match_id
+        print(match_id)
         match = api.get_match(match_id=match_id)
 
         if is_valid(match):
-            print True
+            print(True)
             with open(config.DUMP_DIR + str(match_id) + '.json', 'w+') as f:
                 json.dump(match, f)
             counter += 1
