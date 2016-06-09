e6f0576f33bb6d3984bc8bbcc28870ec4813cb46
Fernando Felix <fernandojr.ifcg@live.com>
2016-06-09 01:17:07 -0300

    Refactor mcounter -> counter_by

--- src/R/utils.R
+++ src/R/utils.R
@@ -96,20 +96,21 @@ discretize = function (x, breaks=NULL) {
     return(cut(x, breaks=breaks))
 }
 
-mcounter = function (x, y) {
-    "Discriminate a dataset x by y to count its subsets.
+counter_by = function (x, y) {
+    "Discriminate a dataset x by y to (distinct) count its subsets.
     "
-    x.domain = unique(x)
-    y.domain = unique(y)
+    x.domain = sort(unique(x))
+    y.domain = sort(unique(y))
     z = matrix(0, nrow=length(y.domain), ncol=length(x.domain))
     colnames(z) = x.domain
     rownames(z) = y.domain
     for (i in range(length(y.domain))) {
-        counts = table(x[y == y.domain[i]])
+        counts = counter(x[y == y.domain[i]])
         for (j in names(counts)) {
             z[i, j] = counts[j]
         }
     }
+
     return(z)
 }
 
