7560dfeb4000b93e8f47a5132aa58356a005a9b7
Fernando Felix <fernandojr.ifcg@live.com>
2016-11-30 01:50:44 -0300

    get api key for collector dynamically

--- src/python/collector.py
+++ src/python/collector.py
@@ -2,7 +2,7 @@
 Collect LoL matches from Riot API at a specific match id range.
 
 How to use:
-python collector.py --start 710229426 --end 728819142 --path ../data/201602
+python collector.py --start 710229426 --end 728819142 --path ../data/201602 --api_key SUA_CHAVE_AQUI
 '''
 import os
 import os.path
@@ -12,14 +12,13 @@ from riotwatcher.riotwatcher import RiotWatcher, BRAZIL, LoLException
 import config
 import random
 
-api_client = RiotWatcher(key=config.API_KEY, default_region=BRAZIL)
-
 
 def valid_match(match, criterion={}):
     return all([match[key] == value for key, value in criterion.items()])
 
 
-def get_match(match_id):
+def get_match(match_id, api_key=config.API_KEY):
+    api_client = RiotWatcher(key=api_key, default_region=BRAZIL)
     return api_client.get_match(match_id=match_id)
 
 
@@ -35,7 +34,8 @@ def save_match(match, path=config.DUMP_DIR):
     return match
 
 
-def collect(start, end, path=config.DUMP_DIR, criterion=config.MATCH_FILTER, n=10000):
+def collect(start, end, path=config.DUMP_DIR, criterion=config.MATCH_FILTER,
+            api_key=config.API_KEY, n=10000):
     collected = []
 
     random_match_id = random.randint(start, end)
@@ -44,7 +44,7 @@ def collect(start, end, path=config.DUMP_DIR, criterion=config.MATCH_FILTER, n=1
             random_match_id = random.randint(start, end)
 
         try:
-            match = get_match(random_match_id)
+            match = get_match(random_match_id, api_key=api_key)
             collected.append(random_match_id)
             if valid_match(match, criterion):
                 save_match(match, path=path)
@@ -66,6 +66,7 @@ if __name__ == '__main__':
     parser.add_option("--start", type=int,  help="starting match id")
     parser.add_option("--end", type=int,  help="ending match id")
     parser.add_option("--path", type=str,  help="path to save macthes")
+    parser.add_option("--api_key", type=str,  help="Riot API key")
     (options, args) = parser.parse_args()
     criterion = {
         'matchMode': 'CLASSIC',
@@ -74,4 +75,4 @@ if __name__ == '__main__':
         'queueType': 'TEAM_BUILDER_DRAFT_RANKED_5x5',
     }
     collect(start=options.start, end=options.end, path=options.path,
-            criterion=criterion)
+            criterion=criterion, api_key=options.api_key)
